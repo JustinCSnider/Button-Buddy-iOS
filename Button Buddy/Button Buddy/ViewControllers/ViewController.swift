8a2cda24ed4864c8762253ff3832afe874d54e23
Justin Snider <jcsnider100@gmail.com>
2021-05-02 07:42:08 -0600

    Initial file structure set up

---
+++ Button Buddy/Button Buddy/ViewControllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Button Buddy
+//
+//  Created by Justin Snider on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    
+    
+    // MARK: - Overrides
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
